0e2f55979e85d3db35cbfb5482293102b91b2a50
Mehedi-irfan <mehediirfan99@gmail.com>
2025-02-23 13:43:55 +0600

    fully completed todo list app

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,31 +1,39 @@
-const submitBtn = document.getElementById("submit-btn");
-submitBtn.addEventListener("click", () => {
-  const inputField = document.getElementById("inputField");
+const inputField = document.getElementById("inputField");
+const listContainer = document.getElementById("list-container");
+
+const addTask = () => {
   if (inputField.value == "") {
-    alert("Please Enter Valid text");
-    return;
+    alert("Please Provide Valid Text");
+  } else {
+    const li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
-  const listContainer = document.getElementById("list-container");
-  const ul = document.createElement("ul");
-  ul.innerHTML = `
-  <li id="checkList" class="text-base text-gray-600 flex items-center  cursor-pointer justify-between">
-  <p class="flex items-center gap-2"><img id="checkImg" class='w-4' src="./images/unchecked.png" />
-  <span id='inputValue'>${inputField.value}</span></p>
-  <button id="close-btn" class="hover:bg-gray-200 px-2 p-1 text-xs rounded-md"><i class="ri-close-line"></i></button>
-  </li>
-  `;
-  listContainer.appendChild(ul);
   inputField.value = "";
+  saveData();
+};
 
-  document.getElementById("checkList").addEventListener("click", () => {
-    const checkImg = document.getElementById("checkImg");
-    checkImg.src = "./images/checked.png";
-    document
-      .getElementById("inputValue")
-      .classList.add("line-through", "text-gray-400");
-  });
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
 
-  document.getElementById("close-btn").addEventListener("click", (e) => {
-    e.target.parentElement.parentElement.remove();
-  });
-});
+const saveData = () => {
+  localStorage.setItem("data", listContainer.innerHTML);
+};
+const displayData = () => {
+  listContainer.innerHTML = localStorage.getItem("data");
+};
+displayData();
